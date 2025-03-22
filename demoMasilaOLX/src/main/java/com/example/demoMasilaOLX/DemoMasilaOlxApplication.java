7eadcdaabd0fef1079e0a11e4f07e962311d906e
masss726 <nmasilamani615@gmail.com>
2025-03-22 20:52:54 +0530

    Add Spring boot Project

---
+++ demoMasilaOLX/src/main/java/com/example/demoMasilaOLX/DemoMasilaOlxApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoMasilaOLX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMasilaOlxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMasilaOlxApplication.class, args);
+	}
+
+}
---
+++ demoMasilaOLX/src/test/java/com/example/demoMasilaOLX/DemoMasilaOlxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoMasilaOLX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMasilaOlxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
